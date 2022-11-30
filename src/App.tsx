42cb8f871d5a00174111f81817f8bbc758c35dc7
poznianski <poznianski@gmail.com>
2022-11-30 11:57:36 +0200

    initial ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ProductCard } from './ProductCard';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header>
         DevStars
       </header>
+      <ProductCard />
     </div>
   );
 }
---
+++ src/ProductCard/ProductCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ProductCard:React.FC = () => {
+  return (
+    <div>
+      ProductCard
+    </div>
+  )
+}
