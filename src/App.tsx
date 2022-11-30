192ed91cb1394e7f96b68cf65b1fffe24879ca47
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-11-30 15:16:01 +0200

    added classes for colors, added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles/main.scss';
 
 function App() {
   return (
     <div>
-      <header>
+      <header className='colorPurple'>
         DevStars
       </header>
     </div>
