360774cce69d464e1af02745433ef1b38f8622da
Sasha Shevchenko <sasha.shevchenko763@gmail.com>
2022-11-30 13:59:00 +0200

    added footer structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Footer } from './Footer';
 import { ProductCard } from './ProductCard';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         DevStars
       </header>
       <ProductCard />
+      <Footer />
     </div>
   );
 }
