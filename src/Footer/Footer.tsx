97a1f116da98a6dbf59d5b27760742d5cc8ef4c4
poznianski <poznianski@gmail.com>
2022-12-01 00:25:35 +0200

    added markup for ProductCard, added reset.css for project, fixed footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './footer_styles/Footer.scss';
 import Up from '../../src/img/Up.svg'
-import Logo from '../../src/img/NiceGadgets.svg'
+import Logo from '../../src/images/NiceGadgets.svg'
 import ok from '../../src/img/ok.svg'
 
 export const Footer: React.FC = () => {
@@ -13,8 +13,8 @@ export const Footer: React.FC = () => {
                 <img src={ok} alt="Logo" className='footer__logo-ok'/>
 
                 </div>
-            
-          
+
+
                 <div className='footer__text-container'>
                     <a href="/" className='footer__text'>GITHUB</a>
                     <a href="/" className='footer__text footer__text-mobile'>CONTACTS</a>
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import styles from './ProductCard.module.scss'
+import phoneDemo from '../../assets/images/phone_demo.png'
 
 export const ProductCard:React.FC = () => {
   return (
-    <div className="product-card">
+    <div className={styles.card}>
       <img
-        className={styles['product-card__image']}
-        src="src/assets/images/phone_demo.png"
+        className={styles.image}
+        src={phoneDemo}
         alt="phone"
       />
 
-      <h2 className="product-card__title">
+      <h2 className={styles.title}>
         Apple iPhone 14 Pro 128GB Silver (MQ023)
       </h2>
 
-      <h3 className="product-card__price">
+      <h3 className={styles.price}>
         $999
       </h3>
 
-      <div className="product-card__line">
+      <span className={styles.line} />
 
+      <div className={styles.info_container}>
+        <p className={styles.info}>
+          <span className={styles.info_name}>Screen</span>
+          <span>6.1” OLED</span>
+        </p>
+
+        <p className={styles.info}>
+          <span className={styles.info_name}>Capacity</span>
+          <span>128 GB</span>
+        </p>
+
+        <p className={styles.info}>
+          <span className={styles.info_name}>RAM</span>
+          <span>6 GB</span>
+        </p>
       </div>
 
-      <div className="product-card__info">
-        <div>
+      <div className={styles.bottom}>
+        <button
+          //TODO: to add href
+          className={styles.buy}
+        >
+          Add to cart
+        </button>
 
-        </div>
+        <div className={styles.favourites} />
       </div>
-
-      <a
-        href="#"
-        className="product-card__buy"
-        data-qa="hover"
-      >
-        Add to cart
-      </a>
-
     </div>
   )
 }
