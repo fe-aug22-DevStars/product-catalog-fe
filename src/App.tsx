f546ac8e1c4f7b1370e65b7b875f821f4b387855
Artem Hulko <agulko96@gmail.com>
2022-12-02 17:14:20 +0200

    added routes for phones and favourites pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './styles/main.scss'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { Catalog } from './components/Catalog'
+import { Favourites } from './components/Favourites'
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
-      <Catalog />
+
+      <Routes>
+        <Route path='phones' element={<Catalog />} />
+        <Route path='favourites' element={<Favourites />} />
+      </Routes>
+
       <Footer />
     </div>
   )
