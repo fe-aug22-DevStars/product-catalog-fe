70a39d223d61fcb334f6c20fa28cce9c0aff47c2
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-12-02 19:19:02 +0200

    Added pagination

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -1,6 +1,7 @@
 import { Phone } from '../types/Phone'
 
 const BASE_URL = 'https://delightful-granita-7b1065.netlify.app/.netlify/functions/server/products'
+// const BASE_URL = 'http://localhost:9000/.netlify/functions/server/products'
 
 export const getAllPhones = async (): Promise<Phone[]> => {
   const response = await fetch(BASE_URL)
@@ -8,7 +9,10 @@ export const getAllPhones = async (): Promise<Phone[]> => {
   return await response.json()
 }
 
-export const getPaginated = async (phonesAmount: number, pageId: number): Promise<Phone[]> => {
+export const getPhones = async (phonesAmount: number, pageId: number): Promise<{
+  'products': Phone[]
+  'numberOfPages': number
+}> => {
   const response = await fetch(`${BASE_URL}/${phonesAmount}/${pageId}`)
 
   return await response.json()
