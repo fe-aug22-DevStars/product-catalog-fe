127ab992cd2c9ec333a10dd9d68123b3bf881cdb
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-01-20 17:21:32 +0200

    Styles were changed

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -33,7 +33,7 @@ export const ProductCard: React.FC<Props> = ({ phone }) => {
           </NavLink>
         </div>
 
-        <NavLink to={`/product/${phone.phoneId}`}>
+        <NavLink className={styles.productLink} to={`/product/${phone.phoneId}`}>
           <h2 className={styles.title}>{phone.name}</h2>
         </NavLink>
 
